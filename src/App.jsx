f5ec967de08f3b2349dae8cca0b946307d4fe869
Shahzaib Fayyaz <shahzaibdev797@gmail.com>
2026-09-15 01:13:15 +0500

    bmw navbar and hero-section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import Navbar from "./Navbar/Navbar";
-import PriceCard from "./React Practice/props";
+import HeroSection from "./Navbar/bmwNavbar";
+// import Navbar from "./Navbar/Navbar";
+// import PriceCard from "./React Practice/props";
 // import { PriceCard } from "./priceCards/priceCard";
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <PriceCard
         name="Pants"
         desc="this is the best pant in the world"
         price="$5"
         rating="4.5"
         img="http://localstorage"
-      />
+      /> */}
+      <HeroSection />
     </>
   );
 }
---
+++ src/Navbar/bmwNavbar.jsx
@@ -0,0 +1,47 @@
+import { CgProfile } from "react-icons/cg";
+import { CiSearch } from "react-icons/ci";
+function HeroSection() {
+  const links = [
+    "Models",
+    "Build Your Own",
+    "Shopping",
+    "Electric",
+    "Owner",
+    "About Us",
+  ];
+  return (
+    <section className=" text-white bg-[url('hero-background.webp')] bg-center bg-cover font-semibold">
+      <nav className="w-full flex items-center justify-between px-10 py-5 border-b-2 border-red">
+        <div className="flex items-center justify-center gap-10">
+          <img src="/navicon.svg" alt="icon" />
+          {links.map((link) => {
+            return <span>{link}</span>;
+          })}
+        </div>
+        <div className="flex items-center gap-5">
+          <a href="#">Choose your location BMW Center</a>
+          <a href="#">
+            <CgProfile size={25} />
+          </a>
+          <a href="#">
+            <CiSearch size={25} />
+          </a>
+        </div>
+      </nav>
+      <div>
+        <h1>LEASE THE 2026</h1>
+        <h1>BMW X5 xDRIVE40i.</h1>
+        <h4>
+          $869 Per month with $6929 due at signing. Plus, loyalty credit upto
+        </h4>
+        <h4>$2000 fir qualified lessees. Now through September 30th.</h4>
+        <div>
+          <button>Offer details</button>
+          <button>See all offers</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
